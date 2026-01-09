6bf35755b45508118659d1d36c3b2172699aa797
Raghav Pillai <raghavpillai101@gmail.com>
2026-01-09 01:46:10 -0800

    Add symlink for .p10k.zsh in setup script
    - create symbolic link for Powerlevel10k config (.p10k.zsh) in user home
    - ensure setup.sh links prompt configuration alongside other dotfiles

--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ ln -sf "$DOTFILES_DIR/.zshrc"     "$HOME/.zshrc"
 ln -sf "$DOTFILES_DIR/.zprofile"  "$HOME/.zprofile"
 ln -sf "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
 ln -sf "$DOTFILES_DIR/.aliases"   "$HOME/.aliases"
+ln -sf "$DOTFILES_DIR/.p10k.zsh"  "$HOME/.p10k.zsh"
 
 mkdir -p "$HOME/.config"
 ln -sf "$DOTFILES_DIR/.config/"* "$HOME/.config/" 2>/dev/null || true
